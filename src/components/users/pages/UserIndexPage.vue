b301dec822ca82666651f346a542e3078603918b
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2024-02-02 05:45:14 +0000

    image added

--- src/components/users/pages/UserIndexPage.vue
+++ src/components/users/pages/UserIndexPage.vue
@@ -1,7 +1,22 @@
 <template>
     <v-row>
         <v-col cols="12" md="6" lg="4">
-            <v-card>
+            <v-card class="elevation-0 py-4">
+                <div class="d-flex" style="justify-content: center">
+                    <div style="width: 150px; height: 150px; background: gray; border-radius: 50%; background-image: url('./user.png')"></div>
+                </div>
+                <v-card-title class="text-center" style="padding: 0px">Nombre de usuario</v-card-title>
+                <v-card-subtitle class="text-center" style="margin-top: -8px">Email</v-card-subtitle>
+            </v-card>
+        </v-col>
+        <v-col cols="12" md="6" lg="4">
+            <v-card class="elevation-0">
+                <v-card-title>Nombre de usuario</v-card-title>
+                <v-card-subtitle>Email</v-card-subtitle>
+            </v-card>
+        </v-col>
+        <v-col cols="12" md="6" lg="4">
+            <v-card class="elevation-0">
                 <v-card-title>Nombre de usuario</v-card-title>
                 <v-card-subtitle>Email</v-card-subtitle>
             </v-card>
