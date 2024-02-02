57120f59f6b4683de14b82eb49f9ba6988617b0d
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2024-02-02 04:05:08 +0000

    basic vuetify components

--- src/components/users/pages/UserIndexPage.vue
+++ src/components/users/pages/UserIndexPage.vue
@@ -1,5 +1,12 @@
 <template>
-    User Index Page
+    <v-row>
+        <v-col cols="12" md="6" lg="4">
+            <v-card>
+                <v-card-title>Nombre de usuario</v-card-title>
+                <v-card-subtitle>Email</v-card-subtitle>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 <script>
 
