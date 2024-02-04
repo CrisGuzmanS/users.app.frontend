3de8c9270d7f79e48287473eaee5c0405c0e154e
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2024-02-04 19:49:45 +0000

    creating interceptor

--- src/backend.js
+++ src/backend.js
@@ -1,8 +1,21 @@
 import axios from "axios";
 import { API_BASE_URL } from "./config";
+import { TokenService } from "./auth/services/TokenService";
 
-const instance = axios.create({
+const backend = axios.create({
     baseURL: API_BASE_URL
 });
 
-export default instance;
+backend.interceptors.request.use((config) => {
+    const token = TokenService.get();
+
+    if (token) {
+        config.headers.Authorization = token;
+    }
+
+    console.log("🟠 token", token);
+
+    return config;
+})
+
+export default backend;
