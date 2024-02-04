3d19982c3d63395fbf9f72af09e9ea955ecc2812
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2024-02-04 04:48:26 +0000

    working with login

---
+++ src/auth/components/pages/LoginPage.vue
@@ -0,0 +1,14 @@
+<template>
+
+    <label>Correo electrónico</label>
+    <v-text-field></v-text-field>
+
+    <label>Contraseña</label>
+    <v-text-field></v-text-field>
+
+    <v-btn>Iniciar sesión</v-btn>
+
+</template>
+<script>
+    export default {};
+</script>
--- src/main.js
+++ src/main.js
@@ -8,13 +8,18 @@ import '@/assets/global.css'
 loadFonts()
 
 import UserPage from '@/users/components/pages/UserIndexPage.vue'
+import LoginPage from '@/auth/components/pages/LoginPage.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [{
         path: '/usuarios',
-        name: 'users',
+        name: 'users.index',
         component: UserPage
+    }, {
+        path: '/login',
+        name: 'login',
+        component: LoginPage
     }]
 })
 
