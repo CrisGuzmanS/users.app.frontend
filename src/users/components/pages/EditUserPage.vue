dc4abdedf3cc4394a43da5406ea1d421c49edc8e
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2024-02-05 03:18:42 +0000

    update user

--- src/users/components/pages/EditUserPage.vue
+++ src/users/components/pages/EditUserPage.vue
@@ -1,14 +1,22 @@
 <template>
 
-    <v-form>
+    <v-form @submit.prevent="onSubmit">
         <v-container>
         
         <v-row>
         
-            <v-col>
+            <v-col cols="12">
 
-                <v-text-field label="Correo electrónico" :value="user.email"></v-text-field>
+                <v-text-field label="Correo electrónico" v-model="user.email"></v-text-field>
             
+            </v-col>
+
+            <v-col cols="12">
+                <v-text-field label="Nombre" v-model="user.name"></v-text-field>
+            </v-col>
+
+            <v-col cols="12">
+                <v-btn type="submit" color="primary">Guardar</v-btn>
             </v-col>
 
         </v-row>
@@ -36,7 +44,17 @@ export default {
     async mounted() {
         const userId = this.$route.params.id;
         this.user = (await backend.get(`usuarios/${userId}`)).data;
-        console.log(this.user.email)
+    },
+
+    methods: {
+        async onSubmit(){
+            const response = await backend.patch(`usuarios/${this.$route.params.id}`, {
+                email: this.user.email,
+                name: this.user.name
+            });
+
+            console.log(response);
+        }
     }
 
 }
